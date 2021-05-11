ed3c89f159992decbe82addb0fdd64a353c925f8
kapil234 <kapil.kumar@samparksolutions.com>
2021-05-11 13:21:24 +0530

    add header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from './container/Header'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import ProductDetail from './container/ProductDetail'
+import ProductList from './container/ProductList'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductList} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 not Found</Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
